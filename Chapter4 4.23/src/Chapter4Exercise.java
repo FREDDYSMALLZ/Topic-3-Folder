664b3c58f8c44ace0b04876ef8fb7fc12459e158
Fredrick Ondieki <freddysmallz@gmail.com>
2017-02-18 23:11:25 -0600

    second commit
    Signed-off-by: Fredrick Ondieki <freddysmallz@gmail.com>

--- Chapter4 4.23/src/Chapter4Exercise.java
+++ Chapter4 4.23/src/Chapter4Exercise.java
@@ -2,40 +2,44 @@ import java.util.Scanner;
 public class Chapter4Exercise {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Financial application: Payroll
 
 		  Scanner input = new Scanner(System.in);
-		  System.out.print("Enter employee's name:");
+		  
+		  //Prompts the user to enter the employee name
+		  
+		  System.out.print("Enter employee name:"); // Name takes a string type input
 		  String name = input.nextLine();
+		  
+		  //Prompts the user to enter the number of hours worked in a week
 		 
 		  System.out.print("Enter number of hours worked in a week:");
 		  double hours = input.nextDouble();
-		 
-		  System.out.print("Enter hourly pay rate:");
+		  System.out.print("Enter hourly pay rate:");// Enter the pay rate per hour
 		  double rate = input.nextDouble();
-		 
-		  System.out.print("Enter federal tax withholding rate:");
-		  double ftax = input.nextDouble();
-		 
-		  System.out.print("Enter state tax withholding rate:");
-		  double stax = input.nextDouble();
-		 
-		  System.out.println("Employee Name:" + name);
-		  System.out.println("Hours Worked:" + hours);
-		  System.out.println("Pay Rate: $" + rate);
-		  System.out.println("Gross Pay: $" + rate * hours);
-		  System.out.println("Deduction:");
-		  System.out.printf("Federal Withholding (%.1f%%): $%.2f\n", ftax * 100,
-		    ftax * rate * hours);
-		  System.out.printf("State Withholding (%.1f%%): $%.2f\n", stax * 100,
-		    stax * rate * hours);
-		  System.out.printf("Total Deduction: $%.2f\n", (stax + ftax) * rate
-		    * hours);
-		  System.out.printf("Net Pay: $%.2f\n", (1 - stax - ftax) * rate * hours);
-		 
-		 }
-		 
-
+		  System.out.print("Enter federal tax withholding rate:");//Enter FED Withholding taxes
+		  double FEDtaxRate = input.nextDouble();
+		  System.out.print("Enter state tax withholding rate:");// Enter the state withholding taxes
+		  double STtaxRate = input.nextDouble();
+		  System.out.println("Employee Name :" + name);//Prints the name of the employee
+		  System.out.println("Hours Worked :" + hours);//Prints the hours worked by employee
+		  System.out.println(" Hourly pay rate:" + " is $" + rate);
+		  
+		  double grosspay = rate * hours ;
+		  double fedWithHolding = ((FEDtaxRate) * grosspay);
+	      double stateWithHolding = ((STtaxRate) * grosspay);
+	      double totalDeduction = fedWithHolding + stateWithHolding;
+	      double netPay = grosspay - totalDeduction;
+	      
+	      System.out.println("  Federal Withholding (" + (double)FEDtaxRate + "%): $" + fedWithHolding);
+	      System.out.println("  State Withholding (" + (double)STtaxRate + "%): $" + stateWithHolding);
+	      System.out.println("  Total Deduction: $" + totalDeduction);
+	      System.out.println("Net Pay: $" + netPay);
 	}
+}
+		 
+		  
+
+	
 
 
