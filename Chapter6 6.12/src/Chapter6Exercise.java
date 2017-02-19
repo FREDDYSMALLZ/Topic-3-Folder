9ccf41a165e675984809d74e10965586eb43180b
Fredrick Ondieki <freddysmallz@gmail.com>
2017-02-19 13:55:00 -0600

    Second commit
    Signed-off-by: Fredrick Ondieki <freddysmallz@gmail.com>

--- Chapter6 6.12/src/Chapter6Exercise.java
+++ Chapter6 6.12/src/Chapter6Exercise.java
@@ -2,18 +2,13 @@
 public class Chapter6Exercise {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		  printChars('1', 'Z', 10);
-	  }
-
+	}
+		// Display characters
+		
+	
 	  public static void printChars(char ch1, char ch2, int numberPerLine) {
-	      int count = 1;
-	      for (int i = ch1; i <= ch2; i++, count++)
-	        if (count % numberPerLine == 0)
-	          System.out.println((char)i);
-	        else
-	          System.out.print((char)i + " ");
+	      
 	  }
 	
 
