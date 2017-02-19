c14af47fff95b2fc1d3cdb041dab788a5209225e
Fredrick Ondieki <freddysmallz@gmail.com>
2017-02-18 21:59:44 -0600

    Second commit
    Signed-off-by: Fredrick Ondieki <freddysmallz@gmail.com>

--- Chapter4 4.11/src/Chapter4Exercise.java
+++ Chapter4 4.11/src/Chapter4Exercise.java
@@ -2,23 +2,28 @@ import java.util.Scanner;
 public class Chapter4Exercise {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Prompt the user to enter integer values between 0-15
+
 		
 		System.out.print("Enter a decimal value (0 to 15): ");
 		  Scanner input = new Scanner(System.in);
 		  int i = input.nextInt();
 		 
-		  if (i < 0 || i > 15) {
-		   System.out.println(i + " is an invalid input");
-		   System.exit(0);
+		  if (i < 0 || i > 15) {            //Using the if statements
+		   System.out.println(i + " is a false input ");
+			   
+		   }
+		  else{
+			  
 		  }
-		 
-		  System.out.println("The hex value is "
+		System.out.println("The hex valid value is " // Displays the corresponding true value 
 		    + Integer.toHexString(i).toUpperCase());
 		 
 		 }
+	
+}
 		 
 		
-	}
+	
 
 
