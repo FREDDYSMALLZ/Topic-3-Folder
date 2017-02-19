e349a09c2579a1fded2e20c64d1f1b2837c3762e
Fredrick Ondieki <freddysmallz@gmail.com>
2017-02-19 16:11:03 -0600

    Second commit
    Signed-off-by: Fredrick Ondieki <freddysmallz@gmail.com>

--- Chapter18 18.17/src/Chapter18Exercise.java
+++ Chapter18 18.17/src/Chapter18Exercise.java
@@ -2,32 +2,15 @@ import java.util.Scanner;
 public class Chapter18Exercise {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+      //Occurrence of a specified character in an array
+		Scanner input = new Scanner(System.in);//Create a scanner
 		
-		Scanner input = new Scanner(System.in);
+		//Prompt the user to enter a list of characters in one line
 		System.out.print("Enter a list of characters in one line: ");
         String s = input.nextLine();
+        //Prompt the user to enter a character
         System.out.print("Enter a character: ");
-        char ch = input.next().charAt(0);
-
-        char[] chars = s.replaceAll(" ", "").toCharArray();
-        System.out.println("The occurrences of '" + ch + "' in \"" + s + "\" = " + count(chars, ch));
-    }
-
-    private static int count(char[] chars, char ch) {
-        return count(chars, ch, chars.length - 1);
-    }
-
-    private static int count(char[] chars, char ch, int high) {
-
-        int count = (chars[high] == ch) ? 1 : 0;
-
-        if (high == 0)
-            return count;
-        else
-            return count + count(chars, ch, high - 1);
-    }
 
 
 	}
-
+}
